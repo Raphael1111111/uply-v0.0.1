645a7905848779c6016de5685fc8ad8aafa153df
Raphael Krebs <raphael.j.krebs@gmail.com>
2023-05-04 22:29:06 +0200

    helene hat geholfen bei firebase verknüpfung lg

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
-// Importieren Sie die benötigten Funktionen aus der Firestore-Bibliothek
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
 
-// Initialisieren Sie Firestore
-const db = getFirestore(app);
+const auth = getAuth(app);
 
-// Funktion zum Hinzufügen eines Benutzers
-async function addUser(userId, name) {
+const loginForm = document.getElementById('login-form');
+const registerBtn = document.getElementById('register');
+
+loginForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
   try {
-    const userRef = await addDoc(collection(db, "users"), {
-      id: userId,
-      name: name,
-    });
-    console.log(`User added with ID: ${userRef.id}`);
-  } catch (e) {
-    console.error("Error adding user: ", e);
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    console.log('User signed in:', userCredential.user);
+  } catch (error) {
+    console.error('Error signing in:', error);
   }
-}
+});
 
-// Rufen Sie die addUser-Funktion auf, um einen Benutzer hinzuzufügen
-addUser("user1", "John Doe");
+registerBtn.addEventListener('click', async () => {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    console.log('User registered:', userCredential.user);
+  } catch (error) {
+    console.error('Error registering user:', error);
+  }
+});
